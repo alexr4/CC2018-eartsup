ae91441ee310b3a61a0d3677dd92738fd15652d9
ALEX-MSI\bonjour <alex@bonjour-lab.com>
2018-05-01 18:45:05 +0200

    no message

--- Cours/8_AnimationAvancee/1_Sprites/JS/sketch.js
+++ Cours/8_AnimationAvancee/1_Sprites/JS/sketch.js
@@ -17,7 +17,7 @@ var time = 0;
 function preload(){
   var nbSprites = 16;
   for(var i=0; i<nbSprites; i++){
-    var sprite = loadImage("../_SRC/Sprites/_EXPORTS/0_Bounce/ball_"+i+".png");
+    var sprite = loadImage("https://github.com/alexr4/CC2018-eartsup/blob/master/Cours/8_AnimationAvancee/_SRC/Sprites/_EXPORTS/0_Bounce/ball_"+i+".png");
     sprites.push(sprite);
   }
 }
